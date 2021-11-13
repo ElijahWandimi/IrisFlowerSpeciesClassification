53f8d233ee2f6ff2e02cd5d11f05920c7204bc08
Elijah Wandimi <wandimiem7@gmail.com>
2021-11-13 20:00:16 +0300

    model

--- CustomFunctions.py
+++ CustomFunctions.py
@@ -1,3 +1,4 @@
+# importing the required packages
 from os import name
 from sklearn.tree import export_graphviz
 from sklearn.datasets import load_iris
@@ -6,27 +7,43 @@ from six import StringIO
 import graphviz, pydotplus
 
 
+# Function to load the data into X and y and get the features names
 def load_data(data):
+    """
+    Loads the data into X and y and returns the features names
+    params:data: the data to be loaded
+    """
     features = data.feature_names
     X = data.data
     y = data.target
     return X, y, features
 
+# Function to split the data into train and test data
 def split_data(X, y, test_size=0.20):
+    """
+    Splits the data into train and test data
+
+    params: X: the data to be split into train and test set data
+            y: the target column to be split into target train and test set
+            test_size: the size of the test set
+    """
     split = int(len(X)*test_size)
     train_X, val_X = X[split:], X[:split]
     train_y, val_y = y[split:], y[:split]
     return train_X, train_y, val_X, val_y
 
+# Function to visualize the decision tree model
 def graph_tree(model, features):
+    """
+    Visualizes the decision tree model
+
+    params: model: the model to be visualized
+            features: the features names to be used in the graph
+    """
     dot_data = StringIO()
     export_graphviz(model, out_file=dot_data,feature_names=features,filled=True,rounded=True)
     graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
     return Image(graph.create_png())
 
-if name == '__main__':
-    data = load_iris()
-    X, y, features = load_data(data, data.feature_names)
-    train_X, train_y, val_X, val_y = split_data(X, y)
-    print(len(train_X), len(train_y), len(val_X), len(val_y))
+
     
--- DecisionTreesModel.py
+++ DecisionTreesModel.py
@@ -1,29 +1,32 @@
+# importing the libraries
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_iris
 from sklearn.metrics import classification_report
 from CustomFunctions import *
 
-
+# setting the random seed
 SEED = 12
 
+# loading the iris dataset
 iris = load_iris()
 
+# loading the data
 X, y, features = load_data(iris)
 
-
+# splitting the data into training and testing sets
 train_X, train_y, test_X, test_y = split_data(X, y, test_size=0.2)
 
-
+# creating the decision tree model
 treeModel = DecisionTreeClassifier(criterion='entropy', max_depth=6, random_state=SEED, min_samples_leaf=5, ccp_alpha=0.034)
 
-
+# fitting the model
 treeModel.fit(train_X, train_y)
 
-
+# visualizing the decision tree
 graph_tree(treeModel, features)
 
-
+# predicting the test set
 preds = treeModel.predict(test_X)
 
-
+# printing the classification report of the decision tree predictions
 print(classification_report(test_y, preds))
